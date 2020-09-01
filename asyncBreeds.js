cd58f934d0fe984bc000dec63f856651edbe7ecc
Danilo Gondim <daniloencarnacao@gmail.com>
2020-09-01 11:47:59 +0000

    Added a test case for our async function

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -1,11 +1,9 @@
 const fs = require('fs');
 
 const breedDetailsFromFile = (breed, callback) => {
-  console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
-    console.log("In readFile's Callback: it has the data.");
     if (!error) callback(data);
   });
 };
 
-breedDetailsFromFile('Bombay', data => console.log('Return Value: ', data));
+module.exports = breedDetailsFromFile;
